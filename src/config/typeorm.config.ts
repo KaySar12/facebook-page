6c8e48c487c33aae44cc787c4f8aea6b0d879655
kaysar <shintheman12@gmail.com>
2023-12-18 22:08:30 +0700

    update

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,13 +1,15 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  url: 'postgresql://postgres:BAaB31CedfB-3dACc5GFedG5-2dd6e-f@monorail.proxy.rlwy.net:14225/railway',
-  type: 'postgres',
-  host: 'monorail.proxy.rlwy.net',
-  port: 14225,
-  username: 'postgres',
-  password: 'BAaB31CedfB-3dACc5GFedG5-2dd6e-f',
-  database: 'railway',
-  entities: [__dirname + '/../**/*.entity.{ts,js}'],
-  synchronize: true,
+  type: 'mssql',
+  host: 'localhost',
+  port: 1433,
+  username: 'admin',
+  password: 'admin',
+  database: 'SocialPage',
+  options: {
+    encrypt: false,
+  },
+  entities: ["dist/**/*.entity{.ts,.js}"],
+  synchronize: false,
 };
 
